fb58b1daf813d2798350383d3b8e7298abea0837
jahbolero <54446274+jahbolero@users.noreply.github.com>
2023-01-05 16:12:21 +0800

    Allow all cors temporarily

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const cors = require('cors');
+const cors = require("cors");
 
-app.use(cors({ origin: process.env.LOCAL_CLIENT}));
+// app.use(cors({ origin: process.env.LOCAL_CLIENT }));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-
-
-app.use(express.json())
+app.use(express.json());
 // Set up the API routes
-const applications = require('./routes/applications');
-app.use('/api', applications);
+const applications = require("./routes/applications");
+app.use("/api", applications);
 
 // Start the server
 var server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-module.exports ={
-  server
-}
+module.exports = {
+  server,
+};
